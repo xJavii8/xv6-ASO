b67ca9aaa42d56f74dcd612a8cdf1d8cb3070940
Javi <jviix8@gmail.com>
2022-11-07 12:27:45 +0100

    Pre-dup2 XV6

---
+++ types.h
@@ -0,0 +1,13 @@
+#ifndef XV6_TYPES_H
+#define XV6_TYPES_H
+
+typedef unsigned int   uint;
+typedef unsigned short ushort;
+typedef unsigned char  uchar;
+typedef uint pde_t;
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#endif
